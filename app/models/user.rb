df4d13053e3595c2a9431aea357b6ad548846e3f
adamtowers <hello@adamtowers.design>
2018-03-26 17:58:14 -0400

    set model relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :user_recipes
+  has_many :user_ingredients
+  has_many :recipes, through: :user_recipes
+  has_many :ingredients, through: :user_ingredients
 end
