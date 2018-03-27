e11062fbe5e32a28aeaec1ee5542b431cfa8e4a1
adamtowers <hello@adamtowers.design>
2018-03-27 16:13:00 -0400

    added User#add_ingredient

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ActiveRecord::Base
   has_many :user_ingredients
   has_many :cocktails, through: :user_cocktails
   has_many :ingredients, through: :user_ingredients
+
+
+  def add_ingredient(ingredient_string)
+    new_ingredient = Ingredient.create_or_find_by(name: ingredient_string.downcase)
+    unless self.ingredients.include?(new_ingredient)
+      self.ingredients << new_ingredient
+    end
+  end
+
+
+
 end
