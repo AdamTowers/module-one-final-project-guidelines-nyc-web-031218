dbcd50a44d963fa6afc383eb45c0c3170819859a
Manuel Grullon <astermanuelg@gmail.com>
2018-03-30 10:21:33 -0400

    cleaned up code

--- bin/run.rb
+++ bin/run.rb
@@ -29,8 +29,8 @@ end
 def run
   prompt = TTY::Prompt.new
   system "clear"
-  puts cocktail_art2.blue.blink
   puts cocktail_art.blue.blink
+  puts cocktail_art2.blue.blink
   puts "Welcome to your personal bar.".white.on_blue
   response = ""
   input = ""
@@ -59,15 +59,11 @@ def run
       end
 
     when "create account"
-      # puts "Please enter your desired username:".yellow
-      # username_request = gets.chomp
       username_request = prompt.ask("Please enter your desire username:".yellow, required: true)
       if User.find_by(username: username_request)
         system "clear"
         puts "Sorry, that username already exists.".white.on_red
       else
-        # puts "Please enter your name".yellow
-        # new_users_name =  gets.chomp
         new_users_name = prompt.ask("Please enter your name".yellow, required: true)
         current_user = User.create(username: username_request, name: new_users_name)
         puts "Welcome, #{current_user.name.titleize}".white.on_blue
@@ -83,8 +79,6 @@ def run
   end
 
   while input
-    # main_menu
-    # input = gets.chomp.downcase
     input = prompt.select("What would you like to do?", main_menu, cycle: true)
     case input
 
@@ -106,8 +100,6 @@ def run
       # puts "The following items are currently in your inventory".yellow
       ingredients = current_user.get_item_names("ingredients")
       ingredients << "cancel"
-      # puts "Please enter the name of the ingredient you would like to delete:".yellow
-      # selected_ingredient = gets.chomp.downcase
       selected_ingredient = prompt.select("Which ingredient would you like to delete?".red, ingredients, cycle: true)
       current_user.delete_ingredient(selected_ingredient.downcase) if selected_ingredient != "cancel"
       puts ""
@@ -121,15 +113,11 @@ def run
 
     when 'add cocktail'
       system "clear"
-      # puts "Please enter the name of the cocktail you would like to save:".yellow
-      # input = gets.chomp.downcase
       input = prompt.ask("Please enter the name of the cocktail you would like to save".yellow, required: true)
       current_user.add_cocktail(input.downcase)
 
     when 'remove cocktail'
       system "clear"
-      # puts "The following cocktails are currently on your favorites".yellow
-      # current_user.get_item_names("cocktails")
       cocktails = current_user.get_item_names("cocktails")
       cocktails << "cancel"
       response = prompt.select("Please select the cocktail you would like to remove from your favorites.".yellow, cocktails, cycle: true)
@@ -161,9 +149,11 @@ def run
 
     when 'search cocktail'
       system "clear"
+      cocktail_names = Cocktail.get_names
+      cocktail_names << "cancel"
       puts "What cocktail would you like to look up?".yellow
-      searched_cocktail = gets.chomp.downcase
-      Cocktail.get_info(searched_cocktail, current_user)
+      searched_cocktail = prompt.select("Which cocktail would you like to select? (Cancel to go back)", cocktail_names, filter: true)
+      Cocktail.get_info(searched_cocktail.downcase, current_user) if searched_cocktail != "cancel"
       puts "==================="
 
     when 'cocktail roulette'
@@ -175,8 +165,6 @@ def run
 
     when 'create cocktail'
       system "clear"
-      # puts "Please input the name of the cocktail you'd like to create:".yellow
-      # input = gets.chomp.downcase
       input = prompt.ask("Please input the name of the cocktail you'd like to create:".yellow, required: true)
       if Cocktail.get_names.include?(input.downcase)
         puts "I'm sorry, a cocktail already exists with that name".white.on_red
@@ -227,34 +215,30 @@ end
 
 def cocktail_art
   <<-'EOF'
-   \
-   .\"""""""""-.
-   \`\-------'`/
-    \ \__ o . /
-     \/  \  o/
-      \__/. /
-       \_ _/
-         Y
-         |
-         |
-     _.-' '-._
-    `---------`
+           \
+           .\"""""""""-.
+           \`\-------'`/
+            \ \__ o . /
+             \/  \  o/
+              \__/. /
+               \_ _/
+                 Y
+                 |
+                 |
+             _.-' '-._
+            `---------`
   EOF
 end
 
 def cocktail_art2
   <<-'EOF'
-   .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.
-  | .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
-  | | ____    ____ | || |  ____  ____  | || |  ____  ____  | || |     ____     | || |   _____      | || |     ____     | || |    ______    | || |  ____  ____  | |
-  | ||_   \  /   _|| || | |_  _||_  _| | || | |_  _||_  _| | || |   .'    `.   | || |  |_   _|     | || |   .'    `.   | || |  .' ___  |   | || | |_  _||_  _| | |
-  | |  |   \/   |  | || |   \ \  / /   | || |   \ \  / /   | || |  /  .--.  \  | || |    | |       | || |  /  .--.  \  | || | / .'   \_|   | || |   \ \  / /   | |
-  | |  | |\  /| |  | || |    \ \/ /    | || |    > `' <    | || |  | |    | |  | || |    | |   _   | || |  | |    | |  | || | | |    ____  | || |    \ \/ /    | |
-  | | _| |_\/_| |_ | || |    _|  |_    | || |  _/ /'`\ \_  | || |  \  `--'  /  | || |   _| |__/ |  | || |  \  `--'  /  | || | \ `.___]  _| | || |    _|  |_    | |
-  | ||_____||_____|| || |   |______|   | || | |____||____| | || |   `.____.'   | || |  |________|  | || |   `.____.'   | || |  `._____.'   | || |   |______|   | |
-  | |              | || |              | || |              | || |              | || |              | || |              | || |              | || |              | |
-  | '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |
-   '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'
+__   __  __   __  __   __  _______  ___      _______  _______  __   __
+|  |_|  ||  | |  ||  |_|  ||       ||   |    |       ||       ||  | |  |
+|       ||  |_|  ||       ||   _   ||   |    |   _   ||    ___||  |_|  |
+|       ||       ||       ||  | |  ||   |    |  | |  ||   | __ |       |
+|       ||_     _| |     | |  |_|  ||   |___ |  |_|  ||   ||  ||_     _|
+| ||_|| |  |   |  |   _   ||       ||       ||       ||   |_| |  |   |
+|_|   |_|  |___|  |__| |__||_______||_______||_______||_______|  |___|
   EOF
 end
 
